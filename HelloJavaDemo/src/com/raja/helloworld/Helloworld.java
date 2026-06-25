c17c61fb6bcb6ba19b16eadd44cf402ad6ed62dd
MooleRaja <mooleraja@gmail.com>
2026-06-25 06:43:33 +0530

    Committ test

--- HelloJavaDemo/src/com/raja/helloworld/Helloworld.java
+++ HelloJavaDemo/src/com/raja/helloworld/Helloworld.java
@@ -3,7 +3,7 @@ package com.raja.helloworld;
 public class Helloworld {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome Rajagopal, Keep gpong with confidence and dially practice");
+		System.out.println("Welcome Rajagopal, Keep gpong with confidence and dially practice....");
 		System.out.println("Welcome Rajagopal, go with consistancy");
 		System.out.println("Hello Java World, I am a memeber of your world !!!!:)");
 	}
